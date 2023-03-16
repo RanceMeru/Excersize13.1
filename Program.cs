99d22dac06ca1941c02449ba3b9ad00c3d641ced
Johnlouie Gutierrez <johgu.829@gmail.com>
2023-03-15 20:35:09 -0700

    This is a slight revision

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Excersze13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //write a program that asks the user to enter a total amount of time in minutes only.
+            //Print to the console the same amount of time in hours and minutes only
+
+
+            Console.WriteLine("Enter your toal  amount in minutes please. ");
+            int userMinutes = int.Parse(Console.ReadLine());
+            int hours = userMinutes / 60;
+            int minutes = userMinutes % 60;
+            Console.WriteLine("Your total time in Hours is {0} hours and {1} minutes", hours, minutes);
+
+        }
+    }
+}
